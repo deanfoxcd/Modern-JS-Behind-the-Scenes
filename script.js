25c7901b2e21b0eea99cf141293370780418c77f
Dean Fox <deanfoxcd@gmail.com>
2024-09-24 20:21:19 -0400

    add first example

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You are ${age} born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Dean';
+calcAge(1991);
