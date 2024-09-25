47cd85fb88ba68076a95c6aeacae72eadffc4a82
Dean Fox <deanfoxcd@gmail.com>
2024-09-24 21:28:18 -0400

    add this keyword with examples

--- script.js
+++ script.js
@@ -72,3 +72,46 @@ function deleteCart() {
   console.log('All products deleted');
 }
   */
+
+// 'This' keyword always points to the owner of the function it's used in
+// Only assigned when function is called
+// Method: this --> Object calling the method
+// Simple function call: this = undefined (in strict mode. Otherwise this --> window)
+// Arrow funcitons: this = this keyword of surrounding function
+// Event listener: this --> DOM element that the handler is attached to
+
+console.log(this); // window
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this); // undefined (doesn't have an owner)
+};
+calcAge(1987);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this); // window (parent scope)
+};
+calcAgeArrow(1937);
+
+const dean = {
+  year: 1977,
+  calcAge: function () {
+    // The console.log will print the dean object. Only because that's the
+    // object calling the function, not because it's written here
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+dean.calcAge();
+
+const belle = {
+  year: 2017,
+};
+
+belle.calcAge = dean.calcAge; // borrows the function
+belle.calcAge();
+// The console.log(this) in the calcAge function in the dean object will print the Belle object because that's what called it
+
+const f = dean.calcAge;
+f(); // this is undefined because there is no onwer, like the first calcAge function
